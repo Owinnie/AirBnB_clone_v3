89773b0b1487c4f39db0f5c4884fb9e311e61acb
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-21 12:16:50 +0300

    tst

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -60,25 +60,17 @@ class DBStorage:
         self.__session.commit()
 
     def get(self, cls, id):
-        """Retrieve one object"""
-        if cls not in classes.values():
+        """Retrieve object by class and id
+        """
+        if cls in classes.values():
+            return self.__session.query(cls).filter(cls.id == id).first()
+        else:
             return None
-        objs_in_cls = models.storage.all(cls)
-        for obj in objs_in_cls.values():
-            if (obj.id == id):
-                return obj
-        return None
 
     def count(self, cls=None):
-        """Count no. of obj in storage"""
-        cnt = 0
-        all_cls_names = classes.values()
-        if not cls:
-            for a_cls_name in all_cls_names:
-                cnt += len(models.storage.all(a_cls_name).values())
-        else:  # if cls
-            cnt += len(models.storage.all(cls).values())
-        return cnt
+        """Count number of objects in storage
+        """
+        return len(self.all(cls))
 
     def delete(self, obj=None):
         """delete from the current database session obj if not None"""
