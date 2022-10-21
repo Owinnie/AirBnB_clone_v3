4ff6582156b821345364b1fda8f52a22cc44ccd3
Owinnie <tillawinnie@gmail.com>
2022-10-21 03:03:57 -0700

    API

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""API Status"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tear_down(error):
+    """Call storage.close()"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, threaded=True)
