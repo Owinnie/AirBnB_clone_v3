99c14294b8ea5fc9b588ca230392897cc5b12569
Owinnie <tillawinnie@gmail.com>
2022-10-23 00:05:20 -0700

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """API Status"""
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -15,6 +15,10 @@ app.register_blueprint(app_views)
 def teardown(error):
     """Call storage.close()"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 
 if __name__ == "__main__":
